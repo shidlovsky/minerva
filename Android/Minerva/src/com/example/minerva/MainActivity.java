21b297eebc70e5d545d47e2fe9a9a84299fad428
Reinis Elksnis <reinis@elksnis.lv>
2014-03-22 12:16:07 +0000

    check connection dialog

--- Android/Minerva/src/com/example/minerva/MainActivity.java
+++ Android/Minerva/src/com/example/minerva/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.minerva;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -8,17 +14,52 @@ import android.webkit.WebViewClient;
 public class MainActivity extends Activity {
 
 	private WebView webView;
+	private Builder builder;
+	private AlertDialog alert;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
 
+		setContentView(R.layout.activity_main);
 		webView = (WebView) findViewById(R.id.webView1);
 		webView.setWebViewClient(new WebViewClient());
 		webView.getSettings().setJavaScriptEnabled(true);
-		webView.loadUrl("http://www.google.com");
 
+		builder = new Builder(this);
+		builder.setTitle("Internet coonection");
+		builder.setMessage("Please connect to the internet");
+		builder.setCancelable(false);
+		builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) {
+				checkConnectionDialog(checkConnection());
+			}
+		});
+		builder.setNegativeButton("Cancel",
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int which) {
+						finish();
+						System.exit(0);
+					}
+				});
+
+		checkConnectionDialog(checkConnection());
+
+	}
+
+	private boolean checkConnection() {
+		NetworkInfo networkInfo = ((ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE))
+				.getActiveNetworkInfo();
+		return (networkInfo != null && networkInfo.isConnected());
+	}
+
+	private void checkConnectionDialog(boolean connection) {
+		if (!connection) {
+			alert = builder.create();
+			alert.show();
+		} else {
+			webView.loadUrl("http://www.google.com");
+		}
 	}
 
 }
